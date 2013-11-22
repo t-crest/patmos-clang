a6e45aa3ae05d2c7765e1198d9c81ff167e7778c
Manman Ren <manman.ren@gmail.com>
2013-11-22 19:42:45 +0000

    Debug Info: add a "Debug Info Version" module flag to output the current debug info version number.
    Will error out when modules have different version numbers.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@195495 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/CodeGenModule.cpp
+++ lib/CodeGen/CodeGenModule.cpp
@@ -260,12 +260,9 @@ void CodeGenModule::Release() {
                               CodeGenOpts.DwarfVersion);
   if (DebugInfo)
     // We support a single version in the linked module: error out when
-    // modules do not have the same version. We are going to implement dropping
-    // debug info when the version number is not up-to-date. Once that is
-    // done, the bitcode linker is not going to see modules with different
-    // version numbers.
+    // modules do not have the same version.
     getModule().addModuleFlag(llvm::Module::Error, "Debug Info Version",
-                              llvm::DEBUG_METADATA_VERSION);
+                              llvm::dwarf::DEBUG_INFO_VERSION);
 
   SimplifyPersonality();
 
