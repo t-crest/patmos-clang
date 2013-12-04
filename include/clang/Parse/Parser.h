2675265ee63ea361b74539026c694afa1213ebf2
Faisal Vali <faisalv@yahoo.com>
2013-12-04 03:51:14 +0000

    Fix PR17637: incorrect calculation of template parameter depth
    In delayed template parsing mode, adjust the template depth counter for each template parameter list associated with an out of line member template specialization.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@196351 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/clang/Parse/Parser.h
+++ include/clang/Parse/Parser.h
@@ -196,6 +196,10 @@ class Parser : public CodeCompletionHandler {
       ++Depth;
       ++AddedLevels;
     }
+    void addDepth(unsigned D) {
+      Depth += D;
+      AddedLevels += D;
+    }
     unsigned getDepth() const { return Depth; }
   };
 
