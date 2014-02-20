d17dacd86c6470c2c08ff9b7498c66dcf7e9a0d2
Reid Kleckner <reid@kleckner.net>
2014-02-20 23:37:45 +0000

    Revert the removal of PPCallbacks::PragmaComment() in r201821
    The pp-trace clang tool was using it successfully.  We can still delete
    the callbacks in Frontend/PrintPreprocessedOutput.cpp because they were
    effectively dead.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@201825 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Parse/ParsePragma.cpp
+++ lib/Parse/ParsePragma.cpp
@@ -1255,6 +1255,11 @@ void PragmaDetectMismatchHandler::HandlePragma(Preprocessor &PP,
     return;
   }
 
+  // If the pragma is lexically sound, notify any interested PPCallbacks.
+  if (PP.getPPCallbacks())
+    PP.getPPCallbacks()->PragmaDetectMismatch(CommentLoc, NameString,
+                                              ValueString);
+
   Actions.ActOnPragmaDetectMismatch(NameString, ValueString);
 }
 
@@ -1324,6 +1329,10 @@ void PragmaCommentHandler::HandlePragma(Preprocessor &PP,
     PP.Diag(Tok.getLocation(), diag::err_pragma_comment_malformed);
     return;
   }
+
+  // If the pragma is lexically sound, notify any interested PPCallbacks.
+  if (PP.getPPCallbacks())
+    PP.getPPCallbacks()->PragmaComment(CommentLoc, II, ArgumentString);
 
   Actions.ActOnPragmaMSComment(Kind, ArgumentString);
 }
