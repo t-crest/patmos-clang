602596b1c0c04e8c02d8d47890e33423d57511c8
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2014-06-20 13:41:24 +0000

    [PowerPC] Add MultiarchIncludeDirs for powerpc64le
    There was already partial support for multi-arch on powerpc64le,
    but the MultiarchIncludeDirs setting was missing.  This patch
    adds the appropriate definition, and also extends the
    linux-header-search.cpp test case to verify an Ubuntu 14.04
    powerpc64le tree.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@211359 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Driver/ToolChains.cpp
+++ lib/Driver/ToolChains.cpp
@@ -3380,6 +3380,9 @@ void Linux::AddClangSystemIncludeArgs(const ArgList &DriverArgs,
   const StringRef PPC64MultiarchIncludeDirs[] = {
     "/usr/include/powerpc64-linux-gnu"
   };
+  const StringRef PPC64LEMultiarchIncludeDirs[] = {
+    "/usr/include/powerpc64le-linux-gnu"
+  };
   ArrayRef<StringRef> MultiarchIncludeDirs;
   if (getTriple().getArch() == llvm::Triple::x86_64) {
     MultiarchIncludeDirs = X86_64MultiarchIncludeDirs;
@@ -3403,6 +3406,8 @@ void Linux::AddClangSystemIncludeArgs(const ArgList &DriverArgs,
     MultiarchIncludeDirs = PPCMultiarchIncludeDirs;
   } else if (getTriple().getArch() == llvm::Triple::ppc64) {
     MultiarchIncludeDirs = PPC64MultiarchIncludeDirs;
+  } else if (getTriple().getArch() == llvm::Triple::ppc64le) {
+    MultiarchIncludeDirs = PPC64LEMultiarchIncludeDirs;
   }
   for (StringRef Dir : MultiarchIncludeDirs) {
     if (llvm::sys::fs::exists(SysRoot + Dir)) {
