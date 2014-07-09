2701638a23bd5e218edee88e9c8012cd09da2d05
Alexey Bataev <a.bataev@hotmail.com>
2014-07-09 09:50:46 +0000

    Revert commit 212599.
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@212600 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/clang/AST/DataRecursiveASTVisitor.h
+++ include/clang/AST/DataRecursiveASTVisitor.h
@@ -2274,9 +2274,8 @@ DEF_TRAVERSE_STMT(AsTypeExpr, {})
 template <typename Derived>
 bool RecursiveASTVisitor<Derived>::TraverseOMPExecutableDirective(
     OMPExecutableDirective *S) {
-  for (auto *C : S->clauses()) {
+  for (auto *C : S->clauses())
     TRY_TO(TraverseOMPClause(C));
-  }
   return true;
 }
 
