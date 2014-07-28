1e0873b8203bd8b3b272e0b90f125e54da2ea6d2
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2014-07-28 13:17:52 +0000

    [PowerPC] Support ELFv1/ELFv2 ABI selection via -mabi= option
    While Clang now supports both ELFv1 and ELFv2 ABIs, their use is currently
    hard-coded via the target triple: powerpc64-linux is always ELFv1, while
    powerpc64le-linux is always ELFv2.
    
    These are of course the most common scenarios, but in principle it is
    possible to support the ELFv2 ABI on big-endian or the ELFv1 ABI on
    little-endian systems (and GCC does support that), and there are some
    special use cases for that (e.g. certain Linux kernel versions could
    only be built using ELFv1 on LE).
    
    This patch implements the Clang side of supporting this, based on the
    LLVM commit 214072.  The command line options -mabi=elfv1 or -mabi=elfv2
    select the desired ABI if present.  (If not, Clang uses the same default
    rules as now.)
    
    Specifically, the patch implements the following changes based on the
    presence of the -mabi= option:
    
    In the driver:
    - Pass the appropiate -target-abi flag to the back-end
    - Select the correct dynamic loader version (/lib64/ld64.so.[12])
    
    In the preprocessor:
    - Define _CALL_ELF to the appropriate value (1 or 2)
    
    In the compiler back-end:
    - Select the correct ABI in TargetInfo.cpp
    - Select the desired ABI for LLVM via feature (elfv1/elfv2)
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@214074 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Driver/Tools.h
+++ lib/Driver/Tools.h
@@ -67,6 +67,8 @@ using llvm::opt::ArgStringList;
                             llvm::opt::ArgStringList &CmdArgs) const;
     void AddMIPSTargetArgs(const llvm::opt::ArgList &Args,
                            llvm::opt::ArgStringList &CmdArgs) const;
+    void AddPPCTargetArgs(const llvm::opt::ArgList &Args,
+                          llvm::opt::ArgStringList &CmdArgs) const;
     void AddR600TargetArgs(const llvm::opt::ArgList &Args,
                            llvm::opt::ArgStringList &CmdArgs) const;
     void AddSparcTargetArgs(const llvm::opt::ArgList &Args,
@@ -388,6 +390,10 @@ namespace mips {
   bool isNaN2008(const llvm::opt::ArgList &Args, const llvm::Triple &Triple);
   bool isFPXXDefault(const llvm::Triple &Triple, StringRef CPUName,
                      StringRef ABIName);
+}
+
+namespace ppc {
+  bool hasPPCAbiArg(const llvm::opt::ArgList &Args, const char *Value);
 }
 
 namespace darwin {
