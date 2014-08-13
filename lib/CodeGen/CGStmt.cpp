bf06478f9a1d365e3d856641645841f46aaa8426
Daniel Prokesch <daniel.prokesch@gmail.com>
2014-08-14 00:30:52 +0200

    loopbound intrinsic: include blockaddress

--- lib/CodeGen/CGStmt.cpp
+++ lib/CodeGen/CGStmt.cpp
@@ -531,6 +531,7 @@ void CodeGenFunction::EmitHeaderBounds(llvm::BasicBlock *Header,
     llvm::Value *MinVal = llvm::ConstantInt::get(Int32Ty, LB->getMin());
     llvm::Value *MaxVal = llvm::ConstantInt::get(Int32Ty, LB->getMax());
 
+    Args.push_back(llvm::BlockAddress::get(Header));
     Args.push_back(MinVal);
     Args.push_back(MaxVal);
     llvm::BasicBlock::iterator I = Header->getFirstInsertionPt();
@@ -538,6 +539,7 @@ void CodeGenFunction::EmitHeaderBounds(llvm::BasicBlock *Header,
     llvm::Value *Callee =
       CGM.getIntrinsic(llvm::Intrinsic::loopbound);
     llvm::CallSite CS = llvm::CallInst::Create(Callee, Args, "", I);
+    (void) CS;
   }
 
 }
