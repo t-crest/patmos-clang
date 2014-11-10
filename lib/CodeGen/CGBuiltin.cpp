4369d4d34f081b7b2f9a798b2f5f632dcb0d2a6f
Alexey Samsonov <vonosmas@gmail.com>
2014-11-10 22:27:30 +0000

    Propagate SanitizerKind into CodeGenFunction::EmitCheck() call.
    Make sure CodeGenFunction::EmitCheck() knows which sanitizer
    it emits check for. Make CheckRecoverableKind enum an
    implementation detail and move it away from header.
    
    Currently CheckRecoverableKind is determined by the type of
    sanitizer ("unreachable" and "return" are unrecoverable,
    "vptr" is always-recoverable, all the rest are recoverable).
    This will change in future if we allow to specify which sanitizers
    are recoverable, and which are not by -fsanitize-recover= flag.
    
    No functionality change.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@221635 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/CGBuiltin.cpp
+++ lib/CodeGen/CGBuiltin.cpp
@@ -493,8 +493,8 @@ RValue CodeGenFunction::EmitBuiltinExpr(const FunctionDecl *FD,
     if (SanOpts.has(SanitizerKind::Unreachable)) {
       SanitizerScope SanScope(this);
       EmitCheck(Builder.getFalse(), "builtin_unreachable",
-                EmitCheckSourceLocation(E->getExprLoc()),
-                None, CRK_Unrecoverable);
+                EmitCheckSourceLocation(E->getExprLoc()), None,
+                SanitizerKind::Unreachable);
     } else
       Builder.CreateUnreachable();
 
