01eb1a6d6be15979065f692dc109e571348a8022
Reid Kleckner <reid@kleckner.net>
2015-02-26 19:43:46 +0000

    Win64: Silently ignore __stdcall, __fastcall, and __thiscall
    MSVC doesn't warn on this. Users are expected to apply the WINAPI macro
    to functions passed by pointer to the Win32 API, and this macro expands
    to __stdcall. This means we end up with a lot of useless noisy warnings
    about ignored calling conventions when compiling code with clang for
    Win64.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@230668 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/clang/Basic/TargetInfo.h
+++ include/clang/Basic/TargetInfo.h
@@ -849,7 +849,8 @@ public:
 
   enum CallingConvCheckResult {
     CCCR_OK,
-    CCCR_Warning
+    CCCR_Warning,
+    CCCR_Ignore,
   };
 
   /// \brief Determines whether a given calling convention is valid for the
