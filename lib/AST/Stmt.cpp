c3630a946c1f54c4356ff44959945c61a681e30c
James Y Knight <jyknight@google.com>
2015-07-17 18:21:37 +0000

    Fix alignment issues in Clang.
    Some const-correctness changes snuck in here too, since they were in the
    area of code I was modifying.
    
    This seems to make Clang actually work without Bus Error on
    32bit-sparc.
    
    Follow-up patches will factor out a trailing-object helper class, to
    make classes using the idiom of appending objects to other objects
    easier to understand, and to ensure (with static_assert) that required
    alignment guarantees continue to hold.
    
    Differential Revision: http://reviews.llvm.org/D10272
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@242554 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/AST/Stmt.cpp
+++ lib/AST/Stmt.cpp
@@ -823,7 +823,7 @@ SourceLocation ObjCAtTryStmt::getLocEnd() const {
 CXXTryStmt *CXXTryStmt::Create(const ASTContext &C, SourceLocation tryLoc,
                                Stmt *tryBlock, ArrayRef<Stmt*> handlers) {
   std::size_t Size = sizeof(CXXTryStmt);
-  Size += ((handlers.size() + 1) * sizeof(Stmt));
+  Size += ((handlers.size() + 1) * sizeof(Stmt *));
 
   void *Mem = C.Allocate(Size, llvm::alignOf<CXXTryStmt>());
   return new (Mem) CXXTryStmt(tryLoc, tryBlock, handlers);
@@ -832,7 +832,7 @@ CXXTryStmt *CXXTryStmt::Create(const ASTContext &C, SourceLocation tryLoc,
 CXXTryStmt *CXXTryStmt::Create(const ASTContext &C, EmptyShell Empty,
                                unsigned numHandlers) {
   std::size_t Size = sizeof(CXXTryStmt);
-  Size += ((numHandlers + 1) * sizeof(Stmt));
+  Size += ((numHandlers + 1) * sizeof(Stmt *));
 
   void *Mem = C.Allocate(Size, llvm::alignOf<CXXTryStmt>());
   return new (Mem) CXXTryStmt(Empty, numHandlers);
@@ -2251,4 +2251,3 @@ OMPTeamsDirective *OMPTeamsDirective::CreateEmpty(const ASTContext &C,
       C.Allocate(Size + sizeof(OMPClause *) * NumClauses + sizeof(Stmt *));
   return new (Mem) OMPTeamsDirective(NumClauses);
 }
-
