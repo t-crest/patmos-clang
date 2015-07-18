a53f1996c2338462f201941bdb531c6f95164b16
Benjamin Kramer <benny.kra@googlemail.com>
2015-07-18 14:35:53 +0000

    [AST] Cleanup ExprIterator.
    - Make it a proper random access iterator with a little help from iterator_adaptor_base
    - Clean up users of magic dereferencing. The iterator should behave like an Expr **.
    - Make it an implementation detail of Stmt. This allows inlining of the assertions.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@242608 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/CodeGenFunction.h
+++ lib/CodeGen/CodeGenFunction.h
@@ -2991,7 +2991,7 @@ public:
                          .getCanonicalType((*I).getNonReferenceType())
                          .getTypePtr() ==
                      getContext()
-                         .getCanonicalType(Arg->getType())
+                         .getCanonicalType((*Arg)->getType())
                          .getTypePtr())) &&
                "type mismatch in call argument!");
         ArgTypes.push_back(*I);
