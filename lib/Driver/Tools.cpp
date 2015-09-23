276497d1032079e50f6042c7c2639dfab7c842f0
Joerg Sonnenberger <joerg@bec.de>
2015-09-23 20:07:56 +0000

    Push OpenMP linker flags after linker input on Darwin. Don't add any libraries if -nostdlib is specified. Test.
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@248424 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -6756,8 +6756,6 @@ void darwin::Linker::ConstructJob(Compilation &C, const JobAction &JA,
 
   Args.AddAllArgs(CmdArgs, options::OPT_L);
 
-  addOpenMPRuntime(CmdArgs, getToolChain(), Args);
-
   AddLinkerInputs(getToolChain(), Inputs, Args, CmdArgs);
   // Build the input file for -filelist (list of linker input files) in case we
   // need it later
@@ -6775,6 +6773,10 @@ void darwin::Linker::ConstructJob(Compilation &C, const JobAction &JA,
 
     InputFileList.push_back(II.getFilename());
   }
+
+  if (!Args.hasArg(options::OPT_nostdlib) &&
+      !Args.hasArg(options::OPT_nodefaultlibs))
+    addOpenMPRuntime(CmdArgs, getToolChain(), Args);
 
   if (isObjCRuntimeLinked(Args) && !Args.hasArg(options::OPT_nostdlib) &&
       !Args.hasArg(options::OPT_nodefaultlibs)) {
