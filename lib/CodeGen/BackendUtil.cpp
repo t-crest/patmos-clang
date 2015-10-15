0ea6bc05696d3797d83f7b6a335c300a3ba0c49b
Teresa Johnson <tejohnson@google.com>
2015-10-15 20:35:53 +0000

    Recommit "Clang support for -flto=thin."
    This recommits r250398 with fixes to the tests for bot failures.
    
    Add "-target x86_64-unknown-linux" to the clang invocations that
    check for the gold plugin.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@250455 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/BackendUtil.cpp
+++ lib/CodeGen/BackendUtil.cpp
@@ -611,8 +611,8 @@ void EmitAssemblyHelper::EmitAssembly(BackendAction Action,
     break;
 
   case Backend_EmitBC:
-    getPerModulePasses()->add(
-        createBitcodeWriterPass(*OS, CodeGenOpts.EmitLLVMUseLists));
+    getPerModulePasses()->add(createBitcodeWriterPass(
+        *OS, CodeGenOpts.EmitLLVMUseLists, CodeGenOpts.EmitFunctionSummary));
     break;
 
   case Backend_EmitLL:
