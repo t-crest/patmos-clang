85fa5fdb5e7050d922a996c925ad482a5686d221
Filipe Cabecinhas <me@filcab.net>
2015-10-16 15:07:48 +0000

    PS4: Make sure to add the sanitizer runtime before any linker input
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@250514 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -10024,6 +10024,8 @@ static void ConstructPS4LinkJob(const Tool &T, Compilation &C,
     assert(Output.isNothing() && "Invalid output.");
   }
 
+  AddPS4SanitizerArgs(ToolChain, CmdArgs);
+
   Args.AddAllArgs(CmdArgs, options::OPT_L);
   Args.AddAllArgs(CmdArgs, options::OPT_T_Group);
   Args.AddAllArgs(CmdArgs, options::OPT_e);
@@ -10041,7 +10043,6 @@ static void ConstructPS4LinkJob(const Tool &T, Compilation &C,
   }
 
   AddPS4ProfileRT(ToolChain, Args, CmdArgs);
-  AddPS4SanitizerArgs(ToolChain, CmdArgs);
 
   const char *Exec = Args.MakeArgString(ToolChain.GetProgramPath("ps4-ld"));
 
@@ -10093,6 +10094,8 @@ static void ConstructGoldLinkJob(const Tool &T, Compilation &C,
   } else {
     assert(Output.isNothing() && "Invalid output.");
   }
+
+  AddPS4SanitizerArgs(ToolChain, CmdArgs);
 
   if (!Args.hasArg(options::OPT_nostdlib) &&
       !Args.hasArg(options::OPT_nostartfiles)) {
@@ -10221,7 +10224,6 @@ static void ConstructGoldLinkJob(const Tool &T, Compilation &C,
   }
 
   AddPS4ProfileRT(ToolChain, Args, CmdArgs);
-  AddPS4SanitizerArgs(ToolChain, CmdArgs);
 
   const char *Exec =
 #ifdef LLVM_ON_WIN32
