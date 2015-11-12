d734145a9a8f759fec119bc01acc05b5cadf160e
Eric Christopher <echristo@gmail.com>
2015-11-12 00:44:04 +0000

    In preparation to use it in more places rename checkBuiltinTargetFeatures to checkTargetFeatures and sink the error handling into the function.
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@252832 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/CodeGenFunction.h
+++ lib/CodeGen/CodeGenFunction.h
@@ -2637,9 +2637,7 @@ public:
   RValue EmitCallExpr(const CallExpr *E,
                       ReturnValueSlot ReturnValue = ReturnValueSlot());
 
-  void getFunctionFeatureMap(llvm::StringMap<bool> &FeatureMap,
-                             const FunctionDecl *FD);
-  bool checkBuiltinTargetFeatures(const FunctionDecl *TargetDecl);
+  void checkTargetFeatures(const CallExpr *E, const FunctionDecl *TargetDecl);
 
   llvm::CallInst *EmitRuntimeCall(llvm::Value *callee,
                                   const Twine &name = "");
